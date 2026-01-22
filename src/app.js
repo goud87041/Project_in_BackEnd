f44cbd82df8eed59b84c5e34c507d434eab45aec
abhishek goud <goud87041@gmail.com>
2026-01-22 07:00:02 +0530

    feat: implement API entry point and establish database connection

--- src/app.js
+++ src/app.js
@@ -1,44 +1,32 @@
 import express from "express"
-// import cors from "cors"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
-
 const app = express()
 
-
 app.use(cors({
-    origin : process.env.CORS_ORIGIN ,
-    credentials  : true
-            
+    origin: process.env.CORS_ORIGIN || "*",
+    credentials: true
 }))
 
-app.use(express.json({limit : "16kb"}))
-app.use(express.urlencoded({extended : true ,limit : "16kb"}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-
-// import routes 
-
+// routes
 import userRouter from "./routes/user.Routes.js"
 import tweetRouter from "./routes/tweet.Routes.js"
 import videoRouter from "./routes/video.Routes.js"
 import commentRouter from "./routes/comment.Routes.js"
 import likeRouter from "./routes/like.Routes.js"
 import playList from "./routes/playList.Routes.js"
-// import { createTweet } from "./controllers/tweet.controllers.js"/
-// import tweetRouter from "./routes/tweet.Routes.js"
 
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comment", commentRouter)
+app.use("/api/v1/like", likeRouter)
+app.use("/api/v1/playList", playList)
 
-// routes decleration
-
-app.use("/api/v1/users",userRouter)
-// app.use("/api/vi",tweetRouter)
-app.use("/api/v1/tweets",tweetRouter)
-app.use("/api/v1/videos",videoRouter)
-app.use("/api/v1/comment",commentRouter)   
-app.use("/api/v1/like",likeRouter)
-app.use("/api/v1/playList",playList)
-
-export {app}
+export { app }
