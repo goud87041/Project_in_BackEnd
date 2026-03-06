4fd114445fa77314c89c83f7bced0e508ff170ed
abhishek goud <goud87041@gmail.com>
2026-03-06 09:42:29 +0530

    fix: update CORS configuration to allow multiple origins

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express()
 // import cors from "cors";
 
 app.use(cors({
-  origin: "https://front-end-project-for-back-end-l2iz.vercel.app",
+  origin: ["https://front-end-project-for-back-end-l2iz.vercel.app", "http://localhost:4000"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 }));
